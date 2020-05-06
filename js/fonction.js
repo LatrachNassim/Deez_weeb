159d3b0ac29ee8e05b18325d60c04196d1845a43
Nassim <latrach.nassim@gmail.com>
2020-05-06 13:14:36 +0200

    Ajout des fichiers css et d'un fichier js

---
+++ js/fonction.js
@@ -0,0 +1,28 @@
+let favorite = [''];
+
+
+
+function addFavorite() {
+    for (let i = 0; i < favorite.length; i++) {
+        if (favorite != undefined) {
+            console.log(favorite[i]);
+        } else {
+            console.log("Vous n'avez aucune musique dans vos favories");
+        }
+    }
+
+    localStorage.setItem("musique", favorite)
+}
+
+function favoriteDelete() {
+    localStorage.removeItem("musique");
+}
+
+function randomMusic() {
+    if(favorite != undefined){
+
+    }else{
+        console.log("Vous n'avez pas de musique favorite");
+    }
+    localStorage.Mathrandom().getItem("musique")
+}
